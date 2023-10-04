57ea696a668d6c6679b6eafc62a64ce64d9ee4e2
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2023-10-04 13:44:24 +0200

    Use https for git clone

--- app/bitcoin/prepare_dependencies.sh
+++ app/bitcoin/prepare_dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-git clone git@github.com:LedgerHQ/vanadium-rust-bitcoin.git rust-bitcoin
-git clone git@github.com:LedgerHQ/vanadium-rust-secp256k1.git rust-secp256k1
+git clone https://github.com/LedgerHQ/vanadium-rust-bitcoin.git rust-bitcoin
+git clone https://github.com/LedgerHQ/vanadium-rust-secp256k1.git rust-secp256k1
